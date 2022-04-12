8637032a9cc6610c0ca278fefc24857d8d6b4327
Anders Hovmöller <anders.hovmoller@dryft.se>
2022-04-12 14:16:04 +0200

    Initial Commit

---
+++ OutputSwitcher/OutputSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutputSwitcherApp.swift
+//  OutputSwitcher
+//
+//  Created by Anders Hovmöller on 2022-04-12.
+//
+
+import SwiftUI
+
+@main
+struct OutputSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
